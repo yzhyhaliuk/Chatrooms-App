ffcf72eee148b0e0fa4a035b2b316827f7324a96
Yana <anazigaluk@gmail.com>
2025-05-29 23:34:30 +0300

    Remove static middleware

--- server/src/index.js
+++ server/src/index.js
@@ -16,12 +16,6 @@ app.use((req, res, next) => {
 
 wss.on('connection', socketHandler);
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
